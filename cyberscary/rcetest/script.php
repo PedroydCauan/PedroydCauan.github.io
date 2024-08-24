c7931983bd95f9271517482ae6ef8a6b89d00cfb
H1i_poopy <129692077+unfunnylaugh@users.noreply.github.com>
2024-08-24 18:33:00 -0300

    Update script.php

--- cyberscary/rcetest/script.php
+++ cyberscary/rcetest/script.php
@@ -5,7 +5,6 @@ $ip        = $_SERVER['REMOTE_ADDR'];
 $useragent = $_SERVER['HTTP_USER_AGENT'];
 
 echo "path of the url (here): $fullpath;<br>
-cuhh ur on doxbin
 file u opened: $filename;<br>
 ya nice IP: $ip;<br>
 browsah agent: $useragent";
